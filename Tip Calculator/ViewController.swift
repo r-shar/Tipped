fc2326bc3cc9b57baaa831f397fac423efda8e03
Rashmi Sharma <rashmisharma@rashmis-macbook-air.local>
2020-04-23 18:24:53 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Rashmi Sharma on 4/23/20.
+//  Copyright © 2020 Rashmi Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
